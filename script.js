dd06ce43d17cbbb63435ccb4875f9701e9b2664d
Grace Do <61570746+grace1501@users.noreply.github.com>
2023-12-20 10:49:30 -0600

    part 2 done

--- script.js
+++ script.js
@@ -1,36 +1,70 @@
 // Part 1: Humble Beginnings
 
-const adventurer = {
-    name: "Robin",
-    health: 10,
-    inventory: ["sword", "potion", "artifact"],
-    companion: {
-        name: "Leo",
-        type: "Cat",
-        companion: {
-            name: "Frank",
-            type: "Flea",
-            belongings: ['small hat', 'sunglasses']
-        }
-    },
+// const adventurer = {
+//     name: "Robin",
+//     health: 10,
+//     inventory: ["sword", "potion", "artifact"],
+//     companion: {
+//         name: "Leo",
+//         type: "Cat",
+//         companion: {
+//             name: "Frank",
+//             type: "Flea",
+//             belongings: ['small hat', 'sunglasses']
+//         }
+//     },
+
+//     roll (mod = 0) {
+//         const result = Math.floor(Math.random() * 20) + 1 + mod;
+//         console.log(`${this.name} rolled a ${result}.`)
+//         }
+//     }
+
+// // a loop that logs each item in Robin’s inventory
+
+// function logInventory(inventoryArr) {
+//     for (let i in inventoryArr) {
+//         console.log(inventoryArr[i]);
+//     }
+//     return true;
+// }
+
+// logInventory(adventurer.inventory);
+// adventurer.roll(0);
+// adventurer.roll(10);
+// adventurer.roll(100);
+
+
+/////////////////////////////////////////////////////////////
+// Part 2: Class Fantasy
+
+class Character {
+    constructor (name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
 
     roll (mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`)
         }
-    }
-
-// a loop that logs each item in Robin’s inventory
-
-function logInventory(inventoryArr) {
-    for (let i in inventoryArr) {
-        console.log(inventoryArr[i]);
-    }
-    return true;
 }
 
-logInventory(adventurer.inventory);
-adventurer.roll(0);
-adventurer.roll(10);
-adventurer.roll(100);
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
+robin.roll();
+robin.roll(10);
+robin.roll(100);
+
+robin.companion.roll();
+robin.companion.companion.roll();
+
+////////////////////////////////
+// Part 3: Class Features
